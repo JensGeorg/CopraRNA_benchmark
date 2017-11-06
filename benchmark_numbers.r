2e6034201e67bfee1c4a101d9c961da1f125340d
JensGeorg <33146097+JensGeorg@users.noreply.github.com>
2017-11-06 14:46:57 +0100

    Update benchmark_numbers.r

--- benchmark_numbers.r
+++ benchmark_numbers.r
@@ -5,37 +5,42 @@ ref<-read.csv("reference.txt" ,sep=";", header=T)
 a<-grep("copraRNA2_ooi_post_filtering.r", d)
 b<-grep("reference.txt", d)
 d<-d[-c(a,b)]
-out<-matrix(,nrow(ref),4)
-colnames(out)<-c("ooi_p_filtered","ooi_p_cons_fltered","ooi_ooi_cons_p_filtered","ooi_cons_p_filtered"
-)
+out<-matrix(,nrow(ref),5)
+colnames(out)<-c("ooi_p_filtered","ooi_p_cons_fltered","ooi_ooi_cons_p_filtered","ooi_cons_p_filtered","IntaRNA_result")
 for(i in 1:length(d)){
-
+	print(i)
 	setwd(paste(wd,"/",d[i],sep=""))
-
-
+	
+	nofil<-read.csv("CopraRNA2_final_all_ooi.csv")
 	fil1<-read.csv("CopraRNA2_final_all_ooi_filtered1.csv")
 	fil2<-read.csv("CopraRNA2_final_all_ooi_filtered2.csv")
-	fil3<-read.csv("CopraRNA2_final_all_ooi_ooi_consensus_filtered.csv") 
+	fil3<-read.csv("CopraRNA2_final_all_ooi_ooi_consensus_filtered.csv")
 	fil4<-read.csv("CopraRNA2_final_all_ooi_consensus_filtered.csv")
-
-
+	e<-grep("Additional.homologs", colnames(fil1))
+	
 
 	temp<-which(ref[,1]==d[i])
 	
 	for(j in 1:length(temp)){
-		temp2<-grep(ref[temp[j],2], fil1[,3])[1]
+		temp2<-grep(ref[temp[j],2],apply(nofil[,3:e], 1, paste, collapse=""))[1]
+		if(length(temp2)>0){
+			out[temp[j],5]<-as.character(nofil[temp2,3])
+		}
+		
+		
+		temp2<-grep(ref[temp[j],2],apply(fil1[,3:e], 1, paste, collapse=""))[1]
 		if(length(temp2)>0){
 			out[temp[j],1]<-temp2
 		}
-		temp2<-grep(ref[temp[j],2], fil2[,3])[1]
+		temp2<-grep(ref[temp[j],2], apply(fil2[,3:e], 1, paste, collapse=""))[1]
 		if(length(temp2)>0){
 			out[temp[j],2]<-temp2
 		}
-		temp2<-grep(ref[temp[j],2], fil3[,3])[1]
+		temp2<-grep(ref[temp[j],2], apply(fil3[,3:e], 1, paste, collapse=""))[1]
 		if(length(temp2)>0){
 			out[temp[j],3]<-temp2
 		}
-		temp2<-grep(ref[temp[j],2], fil4[,3])[1]
+		temp2<-grep(ref[temp[j],2], apply(fil4[,3:e], 1, paste, collapse=""))[1]
 		if(length(temp2)>0){
 			out[temp[j],4]<-temp2
 		}
@@ -46,4 +51,4 @@ for(i in 1:length(d)){
 
 out<-cbind(ref,out)
 
-write.table(out, "benchmark_no_stm.csv", sep=";", quote=F, header=T)
+write.table(out, "benchmark_no_stm.csv", sep=";", quote=F,row.names=F)
