7d911f4b9199a788e892930ac929f98f0610a68a
JensGeorg <33146097+JensGeorg@users.noreply.github.com>
2017-11-08 16:13:22 +0100

    Update benchmark_numbers.r

--- benchmark_numbers.r
+++ benchmark_numbers.r
@@ -4,7 +4,9 @@ d<-dir()
 ref<-read.csv("reference.txt" ,sep=";", header=T)
 a<-grep("copraRNA2_ooi_post_filtering.r", d)
 b<-grep("reference.txt", d)
-d<-d[-c(a,b)]
+dd<-grep("\\.r", d)
+
+d<-d[-c(a,b,dd)]
 out<-matrix(,nrow(ref),9)
 colnames(out)<-c("c2_ooi","c2_ooi_cons","c2_ooi_ooicons","c2_bal","c2_bal_cons","ooi_p_filtered","ooi_p_cons_fltered","ooi_ooi_cons_p_filtered","ooi_cons_p_filtered")
 for(i in 1:length(d)){
@@ -14,10 +16,10 @@ for(i in 1:length(d)){
 	
 	
 	ooi<-read.csv("CopraRNA2_final_all_ooi.csv")
-	ooi_cons<-read.csv("CopraRNA2_final_all_ooi_consensus")
-	ooi_ooi_cons<-read.csv("CopraRNA2_final_ooi_ooiconsensus")
-	bal<-read.csv("CopraRNA2_final_all_balanced")
-	bal_cons<-read.csv("CopraRNA2_final_all_balanced_consensus")
+	ooi_cons<-read.csv("CopraRNA2_final_all_ooi_consensus.csv")
+	ooi_ooi_cons<-read.csv("CopraRNA2_final_ooi_ooiconsensus.csv")
+	bal<-read.csv("CopraRNA2_final_all_balanced.csv")
+	bal_cons<-read.csv("CopraRNA2_final_all_balanced_consensus.csv")
 	fil1<-read.csv("CopraRNA2_final_all_ooi_filtered1.csv")
 	fil2<-read.csv("CopraRNA2_final_all_ooi_filtered2.csv")
 	fil3<-read.csv("CopraRNA2_final_all_ooi_ooi_consensus_filtered.csv")
